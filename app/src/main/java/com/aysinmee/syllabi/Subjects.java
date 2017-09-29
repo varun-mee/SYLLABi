a3e66ec2a1e2dca5adeac2292a6d49b3cd067ade
varun-mee <varunmee@icloud.com>
2017-09-29 21:18:59 +0530

    commit to add new activity content

--- app/src/main/java/com/aysinmee/syllabi/Subjects.java
+++ app/src/main/java/com/aysinmee/syllabi/Subjects.java
@@ -3,6 +3,7 @@ package com.aysinmee.syllabi;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -20,9 +21,9 @@ public class Subjects extends AppCompatActivity {
     public Button sub5;
     public Button sub6;
 
-    public static int value = 0;
+    public static int sem = 0;
 
-    public  String sem1 = "R.raw.sem1_subjects";
+    public  static String data;
 
 
     @Override
@@ -31,7 +32,7 @@ public class Subjects extends AppCompatActivity {
         setContentView(R.layout.activity_subjects);
         getSupportActionBar().setTitle("SUBJECTS");
 
-        value = getIntent().getIntExtra("text",0);
+        sem = getIntent().getIntExtra("text", 0);
 
         sub1 = (Button) findViewById(R.id.sub1);
         sub2 = (Button) findViewById(R.id.sub2);
@@ -42,41 +43,102 @@ public class Subjects extends AppCompatActivity {
 
         InputStream is;
 
-        if(value==1) {
-             is = this.getResources().openRawResource(R.raw.sem1_subjects);
+        if (sem == 1) {
+            is = this.getResources().openRawResource(R.raw.sem1_subjects);
+        } else if (sem == 3) {
+            is = this.getResources().openRawResource(R.raw.sem3_subjects);
+        } else if (sem == 5) {
+            is = this.getResources().openRawResource(R.raw.sem5_subjects);
+        } else {
+            is = this.getResources().openRawResource(R.raw.notext);
         }
-        else if(value==3) {
-             is = this.getResources().openRawResource(R.raw.sem3_subjects);
-        }
-        else if(value==5){
-             is = this.getResources().openRawResource(R.raw.sem5_subjects);
-        }
-        else{
-             is = this.getResources().openRawResource(R.raw.notext);
-        }
-
-        String data = "";
         StringBuffer sb = new StringBuffer();
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(is));
 
-        if(is != null){
-            try{
-                while((data = bufferedReader.readLine()) != null){
+        if (is != null) {
+            try {
+                while ((data = bufferedReader.readLine()) != null) {
                     sb.append(data + "\n");
                 }
                 data = sb.toString();
-                String subs[] = data.split("\\n");
-                sub1.setText(subs[0]);
-                sub2.setText(subs[1]);
-                sub3.setText(subs[2]);
-                sub4.setText(subs[3]);
-                sub5.setText(subs[4]);
-                sub6.setText(subs[5]);
                 is.close();
-            }catch (Exception e){
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
+        init();
+    }
+
+    public void init(){
+
+        String subs[] = data.split("\\n");
+
+        sub1.setText(subs[0]);
+        sub2.setText(subs[1]);
+        sub3.setText(subs[2]);
+        sub4.setText(subs[3]);
+        sub5.setText(subs[4]);
+        sub6.setText(subs[5]);
+
+        sub1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sem = 1;
+                Intent doThis = new Intent(Subjects.this, Content.class);
+                doThis.putExtra("text",sem);
+                startActivity(doThis);
+            }
+        });
+
+        sub2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sem = 2;
+                Intent doThis = new Intent(Subjects.this, Content.class);
+                doThis.putExtra("text",sem);
+                startActivity(doThis);
+            }
+        });
+
+        sub3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sem = 3;
+                Intent doThis = new Intent(Subjects.this, Content.class);
+                doThis.putExtra("text",sem);
+                startActivity(doThis);
+            }
+        });
+
+        sub4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sem = 4;
+                Intent doThis = new Intent(Subjects.this, Content.class);
+                doThis.putExtra("text",sem);
+                startActivity(doThis);
+            }
+        });
+
+        sub5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sem = 5;
+                Intent doThis = new Intent(Subjects.this, Content.class);
+                doThis.putExtra("text",sem);
+                startActivity(doThis);
+            }
+        });
+
+        sub6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sem = 6;
+                Intent doThis = new Intent(Subjects.this, Content.class);
+                doThis.putExtra("text",sem);
+                startActivity(doThis);
+            }
+        });
 
 
     }
